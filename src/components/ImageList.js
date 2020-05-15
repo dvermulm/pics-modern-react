29d5d0a6b3768d130f4b9141ec4a59f66e1a3748
DVM <douglas.vermulm@gmail.com>
2020-05-15 14:07:14 -0400

    Updates through Section 9

---
+++ src/components/ImageList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ImageList = (props) => {
+  const images = props.images.map((image) => {
+    return (
+      <img alt={image.description} key={image.id} src={image.urls.regular} />
+    );
+  });
+
+  return <div>{images}</div>;
+};
+
+export default ImageList;
+
+/* Can destructure this function as follows:
+
+const ImageList = (props) => {
+  const images = props.images.map(( {description, id, urls }) => {
+    return (
+      <img alt={description} key={id} src={urls.regular} />
+    );
+  });
+
+  return <div>{images}</div>;
+};
+
+*/
